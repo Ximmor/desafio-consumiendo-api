446163d8b7f6ed0601623246045379709502468b
Ximena <ximmor@proton.me>
2024-03-28 01:50:03 -0300

    Corregir finales de línea a LF

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="text-center py-14 bg-warning text-danger">
       <h1>
         <i className="fa-brands fa-ligth"></i>
-        <span className="ms-10"> MAGIC API GAMES </span>
+        <span className="ms-2"> MAGIC API GAMES </span>
       </h1>
     </header>
   );
